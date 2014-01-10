eba6102dbd8d48a715541c7b22739aa43d9952d4
Dave Amenta <dave@daveamenta.com>
2014-01-10 12:46:13 -0800

    init

---
+++ RegEdit.psm1
@@ -0,0 +1,17 @@
+﻿function Open-RegEdit {
+	[cmdletbinding()]
+	param($Key)
+	 
+	$Replacements = @{':'='' # Remove PowerShell syntax
+					  'HKCU'='HKEY_CURRENT_USER'
+					  'HKLM'='HKEY_LOCAL_MACHINE'
+					  'HKU'='HKEY_USERS'
+					  'HKCC'='HKEY_CURRENT_CONFIG'
+					  'HKCR'='HKEY_CLASSES_ROOT'}
+	$Replacements.Keys | % {
+		$key = $key.ToUpperInvariant().Replace($_, $Replacements[$_])
+	}
+	Write-Verbose "Open $Key"
+	sp HKCU:\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit\ -Name LastKey -Value "$Key" -Force
+	regedit -m # Open new instance
+}
